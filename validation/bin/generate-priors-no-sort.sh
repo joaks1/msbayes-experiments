fa50e937efb5d1c3219586228754af062de0512a
Jamie Oaks <joaks1@gmail.com>
2013-08-19 18:35:00 -0500

    Adding commands and configs for validation analyses

---
+++ validation/bin/generate-priors-no-sort.sh
@@ -0,0 +1,50 @@
+#! /bin/sh
+#$ -S /bin/bash
+#$ -cwd
+#$ -V
+#$ -l h_vmem=16G
+#$ -l vf=16G
+#$ -q all.q
+#$ -pe orte 8
+
+if [ -n "$SGE_O_WORKDIR" ]
+then
+    source ~/.bash_profile
+    cd /share/work1
+    cd $SGE_O_WORKDIR
+fi
+
+staging_dir=$(mktemp -d /tmp/output.XXXXXXXXX)
+
+reps=1000
+nprocs=8
+nprior=1000000
+batch_size=25000
+nsums=100000
+seed=37851841
+
+output_dir="../no-sort/priors"
+if [ ! -d "$output_dir" ]
+then
+    mkdir -p $output_dir
+fi
+
+dmc.py --np $nprocs \
+    -r 1 \
+    -o ../configs/prior/prior-uniform.cfg \
+    -p ../configs/prior/prior-*.cfg \
+    -n $nprior \
+    --prior-batch-size $batch_size \
+    --num-standardizing-samples $nsums \
+    --sort-index 0 \
+    --output-dir $output_dir \
+    --staging-dir $staging_dir \
+    --seed $seed \
+    --generate-samples-only
+
+echo "Here are the contents of the local temp directory '${staging_dir}':"
+ls -Fla $staging_dir
+echo 'Removing the local temp directory...'
+rm -r $staging_dir
+echo 'Done!'
+
