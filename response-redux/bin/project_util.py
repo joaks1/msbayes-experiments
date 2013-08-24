24ba6817cb3b19a2609c4f302422f9d838e43ce0
Jamie Oaks <joaks1@gmail.com>
2013-08-24 17:13:31 -0500

    Adding shell scripts and configs.
    Adding shell command scripts, python project utilities, and config files
    to the `response-redux` project.

---
+++ response-redux/bin/project_util.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import glob
+
+BIN_DIR = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.abspath(os.path.dirname(BIN_DIR))
+CONFIG_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'configs'))
+DATA_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'sequences'))
+RESULT_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'results'))
+
+def main():
+    sys.stdout.write("%s" % PROJECT_DIR)
+
+if __name__ == '__main__':
+    main()
+
