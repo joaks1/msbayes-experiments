df7ace0bd55e3e2a6fdee14c455ba55a842e3686
Jamie Oaks <joaks1@gmail.com>
2013-09-04 14:21:01 -0500

    Adding shell command script to `model-choice`.
    Adding commands for analyses with `m1.cfg`, and adding commands for
    validation analyses.

---
+++ model-choice/bin/validation-run-reject.sh
@@ -0,0 +1,55 @@
+#! /bin/sh
+#$ -S /bin/bash
+#$ -cwd
+#$ -V
+#$ -l h_vmem=16G
+#$ -l vf=16G
+#$ -q all.q
+#$ -pe orte 8
+
+if [ -n "$SGE_O_WORKDIR" ]
+then
+    source ~/.bash_profile
+    cd /share/work1
+    cd $SGE_O_WORKDIR
+fi
+
+staging_dir=$(mktemp -d /tmp/output.XXXXXXXXX)
+
+reps=10000
+nprocs=8
+nprior=500000
+batch_size=25000
+nsums=100000
+npost=1000
+nquantiles=1000
+seed=91249821
+
+output_dir="../results/validation/results"
+if [ ! -d "$output_dir" ]
+then
+    mkdir -p $output_dir
+fi
+
+dmc.py --np $nprocs \
+    -r $reps \
+    -o ../configs/validation/m[12345].cfg \
+    -p ../results/validation/priors/pymsbayes-results/pymsbayes-output/prior-stats-summaries \
+    -n $nprior \
+    --prior-batch-size $batch_size \
+    --num-posterior-samples $npost \
+    --num-standardizing-samples $nsums \
+    -q $nquantiles \
+    --output-dir $output_dir \
+    --staging-dir $staging_dir \
+    --temp-dir $staging_dir \
+    --seed $seed \
+    --no-global-estimate \
+    --compress
+
+echo "Here are the contents of the local temp directory '${staging_dir}':"
+ls -Fla $staging_dir
+echo 'Removing the local temp directory...'
+rm -r $staging_dir
+echo 'Done!'
+
