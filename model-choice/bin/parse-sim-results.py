69ded0fca31507aa9f3d03a19729da1cbce28876
Jamie Oaks <joaks1@gmail.com>
2013-09-08 13:54:08 -0500

    Updating `parse-sim-results.py`.
    Updating script for parsing results of simulation analyses of the
    `model-choice` project.

--- model-choice/bin/parse-sim-results.py
+++ model-choice/bin/parse-sim-results.py
@@ -6,21 +6,21 @@ import sys
 from pymsbayes.utils.parsing import (DMCSimulationResults,
         parse_posterior_summary_file, dict_line_iter)
 from pymsbayes.config import MsBayesConfig
+from pymsbayes.fileio import process_file_arg
 from pymsbayes.utils.stats import mode_list
 from pymsbayes.utils.messaging import get_logger
 import project_util
 
 _LOG = get_logger(__name__)
-SIM_INFO_PATH = os.path.join(project_util.RESULT_DIR, 'm1-01-sim',
-        'pymsbayes-results', 'pymsbayes-info.txt')
-OUT_PATH = os.path.join(os.path.dirname(SIM_INFO_PATH), 'results.txt')
-NUM_SIMS = 1000
 
 def get_sublist_greater_than(values, threshold):
     return [v for v in values if v > threshold]
 
-def main_cli():
-    sim_results = DMCSimulationResults(SIM_INFO_PATH)
+def parse_sim_results(info_path, num_sims, num_taxon_pairs):
+    sim_results = DMCSimulationResults(info_path)
+    out_dir = os.path.join(os.path.dirname(info_path))
+    summary_path = os.path.join(out_dir, 'results-summary.txt')
+    results_path = os.path.join(out_dir, 'results.txt')
     model_configs = {}
     for k, v in sim_results.prior_index_to_config.iteritems():
         model_configs[k] = MsBayesConfig(v)
@@ -30,12 +30,12 @@ def main_cli():
     ex_tally_glm = 0
     d = {'model_mode': [], 'model_mode_glm': [], 'tau_max': [],
             'tau_max_glm': [], 'num_excluded': [], 'num_excluded_glm': []}
-    for i in range(1, 23):
+    for i in range(1, num_taxon_pairs + 1):
         d['tau_' + str(i)] = []
     for i, (true_params, paths) in enumerate(sim_results.result_path_iter(1,
             '12345-combined')):
         div_times = sorted([float(true_params['PRI.t.' + str(i)]) for i in range(
-                1, 23)])
+                1, num_taxon_pairs + 1)])
         for i, t in enumerate(div_times):
             d['tau_' + str(i + 1)].append(t)
         results = sim_results.get_results_from_params_and_result_paths(
@@ -58,31 +58,41 @@ def main_cli():
         d['tau_max_glm'].append(tau_max_glm)
         d['num_excluded'].append(len(ex))
         d['num_excluded_glm'].append(len(ex_glm))
-    assert len(excluded) == NUM_SIMS
-    assert len(excluded_glm) == NUM_SIMS
+    assert len(excluded) == num_sims
+    assert len(excluded_glm) == num_sims
     for k, v in d.iteritems():
-        assert len(v) == NUM_SIMS, '{0!r} has {1} values'.format(k, len(v))
+        assert len(v) == num_sims, '{0!r} has {1} values'.format(k, len(v))
     assert len([0 for x in excluded if len(x) > 0]) == ex_tally
     assert len([0 for x in excluded_glm if len(x) > 0]) == ex_tally_glm
-    sys.stdout.write('Proportion of simulations excluding truth: {0}\n'.format(
-            ex_tally / float(NUM_SIMS)))
-    sys.stdout.write('Proportion of simulations excluding truth with GLM-'
-            'adjustment: {0}\n'.format(ex_tally_glm / float(NUM_SIMS)))
-    sys.stdout.write('Average number of tau parameters excluded: {0}\n'.format(
-            sum(d['num_excluded']) / float(NUM_SIMS)))
-    sys.stdout.write('Average number of tau parameters excluded with GLM: '
-            '{0}\n'.format(sum(d['num_excluded_glm']) / float(NUM_SIMS)))
-    sys.stdout.write('Mode number of tau parameters excluded: {0}\n'.format(
-            mode_list(d['num_excluded'])))
-    sys.stdout.write('Mode number of tau parameters excluded with GLM: '
-            '{0}\n'.format(mode_list(d['num_excluded_glm'])))
-    sys.stdout.write('Max number of tau parameters excluded: {0}\n'.format(
-            max(d['num_excluded'])))
-    sys.stdout.write('Max number of tau parameters excluded with GLM: '
-            '{0}\n'.format(max(d['num_excluded_glm'])))
-    with open(OUT_PATH, 'w') as out:
-        for line in dict_line_iter(d):
-            out.write(line)
+    with open(summary_path, 'w') as summary_stream:
+        summary_stream.write('Proportion of simulations excluding truth: {0}\n'.format(
+                ex_tally / float(num_sims)))
+        summary_stream.write('Proportion of simulations excluding truth with GLM-'
+                'adjustment: {0}\n'.format(ex_tally_glm / float(num_sims)))
+        summary_stream.write('Average number of tau parameters excluded: {0}\n'.format(
+                sum(d['num_excluded']) / float(num_sims)))
+        summary_stream.write('Average number of tau parameters excluded with GLM: '
+                '{0}\n'.format(sum(d['num_excluded_glm']) / float(num_sims)))
+        summary_stream.write('Mode number of tau parameters excluded: {0}\n'.format(
+                mode_list(d['num_excluded'])))
+        summary_stream.write('Mode number of tau parameters excluded with GLM: '
+                '{0}\n'.format(mode_list(d['num_excluded_glm'])))
+        summary_stream.write('Max number of tau parameters excluded: {0}\n'.format(
+                max(d['num_excluded'])))
+        summary_stream.write('Max number of tau parameters excluded with GLM: '
+                '{0}\n'.format(max(d['num_excluded_glm'])))
+    results_stream, close = process_file_arg(results_path, 'w', compresslevel=9)
+    for line in dict_line_iter(d):
+        results_stream.write(line)
+    results_stream.close()
+
+def main_cli():
+    num_taxon_pairs = 22
+    num_sims = 1000
+    for sim_dir in ['m1-01-sim', 'm1-1-sim']:
+        sim_info_path = os.path.join(project_util.RESULT_DIR, sim_dir,
+                'pymsbayes-results', 'pymsbayes-info.txt')
+        parse_sim_results(sim_info_path, num_sims, num_taxon_pairs)
 
 if __name__ == '__main__':
     main_cli()
