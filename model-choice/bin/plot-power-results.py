2e078746fe50b711e0ab5c5f48b76805fa7265e7
Jamie Oaks <joaks1@gmail.com>
2013-09-10 23:05:46 -0500

    Updating `plot-power-results.py`.
    The script now produces all plots for the `model-choice` power
    analyses.

--- model-choice/bin/plot-power-results.py
+++ model-choice/bin/plot-power-results.py
@@ -5,14 +5,12 @@ import sys
 
 from pymsbayes.utils.parsing import DMCSimulationResults, spreadsheet_iter
 from pymsbayes.config import MsBayesConfig
-from pymsbayes.plotting import PsiPowerPlotGrid
+from pymsbayes.plotting import (PowerPlotGrid, ProbabilityPowerPlotGrid,
+        AccuracyPowerPlotGrid)
 from pymsbayes.utils.messaging import get_logger
 import project_util
 
 _LOG = get_logger(__name__)
-POWER_INFO_PATH = os.path.join(project_util.RESULT_DIR, 'power',
-        'pymsbayes-results', 'pymsbayes-info.txt')
-RESULT_DIR = os.path.dirname(POWER_INFO_PATH)
 
 class PowerResult(object):
     def __init__(self):
@@ -58,17 +56,77 @@ def parse_results(info_path):
         omega_results[cfg] = omega
     return psi_results, omega_results
 
-def main_cli():
-    psi_results, omega_results = parse_results(POWER_INFO_PATH)
+def create_plots(info_path):
+    output_dir = os.path.join(os.path.dirname(info_path), 'plots')
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    prior_prob_omega_less_than = 0.0887
+    psi_results, omega_results = parse_results(info_path)
     cfg_to_psi = {}
+    cfg_to_psi_prob = {}
     for cfg, psi in psi_results.iteritems():
         cfg_to_psi[cfg] = psi.mode
+        cfg_to_psi_prob[cfg] = psi.prob
+    cfg_to_omega = {}
+    cfg_to_omega_prob = {}
+    cfg_to_omega_true_ests = {}
+    cfg_to_omega_true_ests_glm = {}
+    for cfg, omega in omega_results.iteritems():
+        cfg_to_omega[cfg] = omega.median
+        cfg_to_omega_prob[cfg] = omega.prob
+        cfg_to_omega_true_ests[cfg] = {'x': omega.true, 'y': omega.median}
+        cfg_to_omega_true_ests_glm[cfg] = {'x': omega.true, 'y': omega.mode_glm}
 
-    psi_plot = PsiPowerPlotGrid(observed_config_to_estimates = cfg_to_psi,
-            num_taxon_pairs = 22,
+    psi_plot = PowerPlotGrid(observed_config_to_estimates = cfg_to_psi,
+            variable = 'psi',
             num_columns = 2)
     fig = psi_plot.create_grid()
-    fig.savefig('psi_power.pdf')
+    fig.savefig(os.path.join(output_dir, 'power_psi_mode.pdf'))
+
+    psi_prob_plot = ProbabilityPowerPlotGrid(
+            observed_config_to_estimates = cfg_to_psi_prob,
+            variable = 'psi',
+            div_model_prior = 'psi',
+            bayes_factor = 10,
+            num_columns = 2)
+    fig = psi_prob_plot.create_grid()
+    fig.savefig(os.path.join(output_dir, 'power_psi_prob.pdf'))
+
+    omega_plot = PowerPlotGrid(observed_config_to_estimates = cfg_to_omega,
+            variable = 'omega',
+            num_columns = 2)
+    fig = omega_plot.create_grid()
+    fig.savefig(os.path.join(output_dir, 'power_omega_median.pdf'))
+
+    omega_prob_plot = ProbabilityPowerPlotGrid(
+            observed_config_to_estimates = cfg_to_omega_prob,
+            variable = 'omega',
+            div_model_prior = 'psi',
+            bayes_factor = 10,
+            bayes_factor_prob = prior_prob_omega_less_than,
+            num_columns = 2)
+    fig = omega_prob_plot.create_grid()
+    fig.savefig(os.path.join(output_dir, 'power_omega_prob.pdf'))
+    
+    omega_accuracy_plot = AccuracyPowerPlotGrid(
+            observed_config_to_estimates = cfg_to_omega_true_ests,
+            num_columns = 2)
+    fig = omega_accuracy_plot.create_grid()
+    fig.savefig(os.path.join(output_dir, 'power_accuracy_omega_median.pdf'))
+
+    omega_accuracy_plot_glm = AccuracyPowerPlotGrid(
+            observed_config_to_estimates = cfg_to_omega_true_ests_glm,
+            num_columns = 2)
+    fig = omega_accuracy_plot_glm.create_grid()
+    fig.savefig(os.path.join(output_dir, 'power_accuracy_omega_mode_glm.pdf'))
+
+def main_cli():
+    if len(sys.argv) != 2:
+        sys.stderr.write('This script requires one argument: The path to the '
+                'power analysis `pymsbayes-info.txt` file')
+        sys.exit(1)
+    info_path = sys.argv[1]
+    create_plots(info_path)
 
 if __name__ == '__main__':
     main_cli()
