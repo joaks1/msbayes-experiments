d2ae99680d888be875daeba31832f0ad79ac7020
Jamie Oaks <joaks1@gmail.com>
2013-09-15 02:46:26 -0500

    Adding uniform validation results.
    Also, adding dpp and uniform plotting scripts.

---
+++ validation/bin/plot-dpp-results.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+import os
+import sys
+
+from pymsbayes import plotting
+import project_util
+
+def main_cli():
+    info_path = os.path.join(project_util.RESULT_DIR, 'dpp',
+                'pymsbayes-results', 'pymsbayes-info.txt')
+    plotting.plot_validation_results(info_path)
+
+if __name__ == '__main__':
+    main_cli()
+
