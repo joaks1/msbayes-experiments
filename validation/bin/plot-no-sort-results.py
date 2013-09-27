db40d6f577051855edc72a64ff4267b2eac70bc5
Jamie Oaks <joaks1@gmail.com>
2013-09-27 12:41:10 -0500

    Updating validation plotting scripts.
    Plots using new notation of dpp ms.

--- validation/bin/plot-no-sort-results.py
+++ validation/bin/plot-no-sort-results.py
@@ -9,7 +9,11 @@ import project_util
 def main_cli():
     info_path = os.path.join(project_util.NO_SORT_DIR, 'results',
                 'pymsbayes-results', 'pymsbayes-info.txt')
-    plotting.plot_validation_results(info_path)
+    plotting.plot_validation_results(info_path,
+            omega_symbol = r'D_T',
+            psi_symbol = r'|\mathbf{\tau}|',
+            mean_time_symbol = r'\bar{T}',
+            math_font = None)
 
 if __name__ == '__main__':
     main_cli()
