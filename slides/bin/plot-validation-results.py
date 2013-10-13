7dc50464ebacaf64e7475e37db6be0e9b40453df
Jamie Oaks <joaks1@gmail.com>
2013-10-12 23:45:43 -0500

    Working on `plot-validation-results.py`
    Working on script for generating plots of validation results
    for use in slides.

--- slides/bin/plot-validation-results.py
+++ slides/bin/plot-validation-results.py
@@ -42,7 +42,88 @@ def main_cli():
             sub_plots[i] = {}
         for j in ['old', 'dpp', 'uniform', 'u-shaped']:
             sub_plots[i][j] = results['prior-' + i]['prior-' + j].prob_plot.subplots[0]
-            print sub_plots[i][j]
+            sub_plots[i][j].set_xlabel('')
+            sub_plots[i][j].set_ylabel('')
+
+    sub_plot_list = [
+            sub_plots['old']['old'],
+            sub_plots['old']['dpp'],
+            sub_plots['dpp']['old'],
+            sub_plots['dpp']['dpp'],
+            ]
+
+    pg = plotting.PlotGrid(subplots = sub_plot_list,
+            num_columns = 2,
+            share_x = True,
+            share_y = True,
+            title = 'Posterior probability of one divergence',
+            title_size = 12.0,
+            title_top = False,
+            y_title = 'True probability of one divergence',
+            y_title_position = 0.03,
+            y_title_size = 12.0,
+            height = 6.0,
+            auto_height = False,
+            column_labels = [r'$M_{msBayes}$', r'$M_{DPP}$'],
+            row_labels = [r'$M_{msBayes}$', r'$M_{DPP}$'],
+            column_label_size = 20.0,
+            row_label_size = 20.0,
+            column_label_offset = 0.05,
+            row_label_offset = 0.05,
+            super_title = r'\textbf{Analysis model}',
+            super_y_title = r'\textbf{Data model}',
+            super_title_size = 20.0,
+            super_y_title_size = 20.0,
+            super_y_title_right = True)
+    pg.label_schema = None
+    pg.auto_adjust_margins = False
+    pg.margin_bottom = 0.04
+    pg.margin_left = 0.06
+    pg.margin_top = 0.90
+    pg.margin_right = 0.92
+    pg.reset_figure()
+
+    pg.savefig('../images/validation-model-choice-old-dpp.pdf')
+
+    sub_plot_list.extend([
+        sub_plots['uniform']['old'],
+        sub_plots['uniform']['dpp'],
+        sub_plots['u-shaped']['old'],
+        sub_plots['u-shaped']['dpp'],
+        ])
+
+    pg = plotting.PlotGrid(subplots = sub_plot_list,
+            num_columns = 2,
+            share_x = True,
+            share_y = True,
+            title = 'Posterior probability of one divergence',
+            title_size = 12.0,
+            title_top = False,
+            y_title = 'True probability of one divergence',
+            y_title_position = 0.03,
+            y_title_size = 12.0,
+            height = 10.2,
+            auto_height = False,
+            column_labels = [r'$M_{msBayes}$', r'$M_{DPP}$'],
+            row_labels = [r'$M_{msBayes}$', r'$M_{DPP}$', r'$M_{Uniform}$', r'$M_{Ushaped}$'],
+            column_label_size = 20.0,
+            row_label_size = 20.0,
+            column_label_offset = 0.05,
+            row_label_offset = 0.05,
+            super_title = r'\textbf{Analysis model}',
+            super_y_title = r'\textbf{Data model}',
+            super_title_size = 20.0,
+            super_y_title_size = 20.0,
+            super_y_title_right = True)
+    pg.label_schema = None
+    pg.auto_adjust_margins = False
+    pg.margin_bottom = 0.02
+    pg.margin_left = 0.06
+    pg.margin_top = 0.95
+    pg.margin_right = 0.92
+    pg.reset_figure()
+
+    pg.savefig('../images/validation-model-choice-old-dpp-full.pdf')
 
 if __name__ == '__main__':
     main_cli()
