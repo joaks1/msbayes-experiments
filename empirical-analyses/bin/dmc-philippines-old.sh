49b398d3f93d9b96ca21e41763ce6cec4f8dd5c3
Jamie Oaks <joaks1@gmail.com>
2013-11-08 15:08:48 -0800

    Adding config and command-line script.
    Adding analysis files to `empirical-analyses` for analyses of the
    Philippines data under the old msBayes implementation.

---
+++ empirical-analyses/bin/dmc-philippines-old.sh
@@ -0,0 +1,57 @@
+#! /bin/sh
+#$ -S /bin/bash
+#$ -cwd
+#$ -V
+#$ -l h_vmem=16G
+#$ -l vf=16G
+#$ -q all.q
+#$ -pe orte 8
+
+if [ -n "$SGE_O_WORKDIR" ]
+then
+    source ~/.bash_profile
+    cd /share/work1
+    cd $SGE_O_WORKDIR
+fi
+
+staging_dir=$(mktemp -d /tmp/output.XXXXXXXXX)
+
+
+nprocs=8
+nprior=20000000
+batch_size=25000
+nsums=200000
+npost=10000
+nquantiles=10000
+reporting_freq=20
+sort_index=7
+seed=238691091
+
+output_dir="../results/philippines/old"
+if [ ! -d "$output_dir" ]
+then
+    mkdir -p $output_dir
+fi
+
+dmc.py --np $nprocs \
+    -o ../configs/philippines-old.cfg \
+    -p ../configs/philippines-old.cfg \
+    -n $nprior \
+    --prior-batch-size $batch_size \
+    --num-posterior-samples $npost \
+    --num-standardizing-samples $nsums \
+    -q $nquantiles \
+    --reporting-frequency $reporting_freq \
+    --sort-index $sort_index \
+    --output-dir $output_dir \
+    --staging-dir $staging_dir \
+    --temp-dir $staging_dir \
+    --compress \
+    --seed $seed
+
+echo "Here are the contents of the local temp directory '${staging_dir}':"
+ls -Fla $staging_dir
+echo 'Removing the local temp directory...'
+rm -r $staging_dir
+echo 'Done!'
+
