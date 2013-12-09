14bfa2270811dcb43b2a90321f9fb694f64fa72b
Jamie Oaks <joaks1@gmail.com>
2013-12-08 23:50:37 -0800

    Adding plotting script to `valication` project.
    Adding `plot-valication-comparison.py` plotting script for plotting
    model-choice behavior plots that compare across tested models.

---
+++ validation/bin/plot-validation-comparison.py
@@ -0,0 +1,119 @@
+#! /usr/bin/env python
+
+import os
+import sys
+
+from pymsbayes import plotting
+import project_util
+
+def main_cli():
+    results = {}
+    for d in ['dpp', 'old', 'uniform', 'u-shaped']:
+        info_path = os.path.join(project_util.RESULT_DIR, d,
+                    'pymsbayes-results', 'pymsbayes-info.txt')
+        r = plotting.plot_validation_results(info_path,
+                plot_dir = project_util.IMAGE_DIR,
+                omega_symbol = r'D_T',
+                psi_symbol = r'|\mathbf{\tau}|',
+                mean_time_symbol = r'\bar{T}',
+                plot_accuracy = False,
+                prob_plot_glm = True,
+                prob_plot_height = 3.5,
+                prob_plot_margin_left = 0,
+                prob_plot_margin_bottom = 0,
+                prob_plot_margin_right = 1,
+                prob_plot_margin_top = 0.975,
+                prob_plot_padding_between_horizontal = 0.5,
+                prob_plot_padding_between_vertical = 1.0,
+                math_font = None,
+                write_plots = False)
+        for obs_name, p_dict in r.iteritems():
+            for prior_name, validation_result in p_dict.iteritems():
+                if not results.has_key(obs_name):
+                    results[obs_name] = {}
+                if not results[obs_name].has_key(prior_name):
+                    results[obs_name][prior_name] = validation_result
+                else:
+                    raise Exception('Unexpected duplicate validation result')
+
+    sub_plot_map = {
+            0: ('psi', (r'Posterior probability of one divergence---'
+                r'$p(|\mathbf{\tau}| = 1 \, | \, B_{{\epsilon}}(S*))$')),
+            2: ('psi-glm', (r'GLM-adjusted posterior probability of one divergence---'
+                r'$p(|\mathbf{\tau}| = 1 \, | \, B_{{\epsilon}}(S*))$')),
+            1: ('omega', (r'Posterior probability of one divergence---'
+                r'$p(D_T < 0.01 \, | \, B_{{\epsilon}}(S*))$')),
+            3: ('omega-glm', (r'GLM-adjusted posterior probability of one divergence---'
+                r'$p(D_T < 0.01 \, | \, B_{{\epsilon}}(S*))$')),
+                }
+    for idx, (variable, x_label) in sub_plot_map.iteritems():
+        sub_plots = {}
+        for i in ['old', 'dpp', 'uniform', 'u-shaped']:
+            if not sub_plots.has_key(i):
+                sub_plots[i] = {}
+            for j in ['old', 'dpp', 'uniform', 'u-shaped']:
+                sub_plots[i][j] = results['prior-' + i]['prior-' + j].prob_plot.subplots[idx]
+                sub_plots[i][j].set_xlabel('')
+                sub_plots[i][j].set_ylabel('')
+
+        sub_plot_list = [
+                sub_plots['old']['old'],
+                sub_plots['u-shaped']['old'],
+                sub_plots['uniform']['old'],
+                sub_plots['dpp']['old'],
+                sub_plots['old']['u-shaped'],
+                sub_plots['u-shaped']['u-shaped'],
+                sub_plots['uniform']['u-shaped'],
+                sub_plots['dpp']['u-shaped'],
+                sub_plots['old']['uniform'],
+                sub_plots['u-shaped']['uniform'],
+                sub_plots['uniform']['uniform'],
+                sub_plots['dpp']['uniform'],
+                sub_plots['old']['dpp'],
+                sub_plots['u-shaped']['dpp'],
+                sub_plots['uniform']['dpp'],
+                sub_plots['dpp']['dpp'],
+                ]
+        for p in sub_plot_list:
+            p.set_extra_y_label('')
+            for scatter_data in p.scatter_data_list:
+                scatter_data.markersize = 5.0
+
+        pg = plotting.PlotGrid(subplots = sub_plot_list,
+                num_columns = 4,
+                share_x = True,
+                share_y = True,
+                title = x_label,
+                title_size = 14.0,
+                title_top = False,
+                y_title = 'True probability',
+                y_title_position = 0.015,
+                y_title_size = 14.0,
+                height = 9.0,
+                width = 11.0,
+                auto_height = False,
+                row_labels = [r'$M_{msBayes}$', r'$M_{Ushaped}$', r'$M_{Uniform}$', r'$M_{DPP}$'],
+                column_labels = [r'$M_{msBayes}$', r'$M_{Ushaped}$', r'$M_{Uniform}$', r'$M_{DPP}$'],
+                column_label_size = 20.0,
+                row_label_size = 20.0,
+                column_label_offset = 0.05,
+                row_label_offset = 0.05,
+                super_title = r'\textbf{Data model}',
+                super_y_title = r'\textbf{Analysis model}',
+                super_title_size = 20.0,
+                super_y_title_size = 20.0,
+                super_y_title_right = True)
+        # pg.label_schema = None
+        pg.auto_adjust_margins = False
+        pg.margin_bottom = 0.04
+        pg.margin_left = 0.04
+        pg.margin_top = 0.94
+        pg.margin_right = 0.94
+        pg.plot_label_size = 12.0
+        pg.reset_figure()
+
+        pg.savefig('../images/validation-model-choice-summary-' + variable + '.pdf')
+
+if __name__ == '__main__':
+    main_cli()
+
