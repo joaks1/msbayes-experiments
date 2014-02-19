b052aedf0351c4376ad4e561dafb007633149b13
Jamie Oaks <joaks1@gmail.com>
2014-02-19 15:38:28 -0800

    Adding `prior-tests`.

---
+++ prior-tests/bin/generate-priors-no-sort.sh
@@ -0,0 +1,51 @@
+#! /bin/sh
+#$ -S /bin/bash
+#$ -cwd
+#$ -V
+#$ -l h_vmem=16G
+#$ -l vf=16G
+#$ -q all.q
+#$ -pe orte 8
+
+if [ -n "$SGE_O_WORKDIR" ]
+then
+    source ~/.bash_profile
+    cd /share/work1
+    cd $SGE_O_WORKDIR
+fi
+
+staging_dir=$(mktemp -d /tmp/output.XXXXXXXXX)
+
+nprocs=8
+nprior=10000
+batch_size=100
+nsums=10000
+seed=37851841
+
+output_dir="../priors"
+if [ ! -d "$output_dir" ]
+then
+    mkdir -p $output_dir
+fi
+
+dmc.py --np $nprocs \
+    -r 1 \
+    -o ../configs/equal.cfg \
+    -p ../configs/*.cfg \
+    -n $nprior \
+    --prior-batch-size $batch_size \
+    --num-standardizing-samples $nsums \
+    --sort-index 0 \
+    --output-dir $output_dir \
+    --staging-dir $staging_dir \
+    --temp-dir $staging_dir \
+    --seed $seed \
+    --compress \
+    --generate-samples-only
+
+echo "Here are the contents of the local temp directory '${staging_dir}':"
+ls -Fla $staging_dir
+echo 'Removing the local temp directory...'
+rm -r $staging_dir
+echo 'Done!'
+
